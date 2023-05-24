3839e4cb0407708d67812b5457783b9094abdb31
dermeidl <132459670+dermeidl@users.noreply.github.com>
2023-05-24 12:25:56 +0000

    working fine

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Linz',
+  'salary': '5.000€'
+}, {
+  'id': 2,
+  'title': 'Data Engeneer',
+  'location': 'Linz',
+  'salary': '5.000€'
+}, {
+  'id': 3,
+  'title': 'Frontend Engeneer',
+  'location': 'Remote',
+  'salary': '5.000€'
+}]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='NeoClarity')
+
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
